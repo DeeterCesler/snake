74a5f283b926b49d2464283283df2938d410d2df
Deeter <deeter.cesler@gmail.com>
2019-02-02 23:03:38 -0700

    flask pulls saved scores from MySQL

--- app.py
+++ app.py
@@ -21,33 +21,6 @@ def scores():
     rv = cur.fetchall()
     return jsonify(rv)
     
-# tasks = [
-#     {
-#         'id': 1,
-#         'title': u'Buy groceries',
-#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-#         'done': False
-#     },
-#     {
-#         'id': 2,
-#         'title': u'Learn Python',
-#         'description': u'Need to find a good Python tutorial on the web', 
-#         'done': False
-#     }
-# ]
-
-# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
-# def get_tasks():
-#     return jsonify({'tasks': tasks})
-
-# @app.route('/todo/api/v1.0/tasks/<int:num>', methods=['GET'])
-# def get_task(num):
-#     return jsonify({'tasks': tasks[num]})
-
- 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
  
 if __name__ == "__main__":
     app.run()
