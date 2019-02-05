0cd3c36a031d06454a070fd4352f11e6f25cac83
Deeter <deeter.cesler@gmail.com>
2019-02-05 13:48:09 -0700

    PORT! now with more port

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def newScore():
     return jsonify(rv)
  
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
