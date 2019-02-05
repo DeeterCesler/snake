24b3407def231db621972dfa5fa5a36686bba308
Deeter <deeter.cesler@gmail.com>
2019-02-05 16:44:55 -0700

    template correction

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ cursor = conn.cursor()
  
 @app.route("/")
 def index():
-    return render_template("index.1.html")
+    return render_template("index.html")
 
 @app.route("/scoreboard")
 def scores():
